3625c94538cd0cf047797246e4e80c6f6dfa42b4
xlcod <alvaro.pernas@live.u-tad.com>
2019-11-08 14:39:54 +0100

    anador adapter

--- RetoMapsSave/app/src/main/java/com/example/retomapssave/MapsActivity.kt
+++ RetoMapsSave/app/src/main/java/com/example/retomapssave/MapsActivity.kt
@@ -101,9 +101,6 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback,GoogleMap.OnMarkerC
         mMap = googleMap
 
         // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
 
         mMap.setOnMarkerClickListener(this)
         mMap.uiSettings.isZoomControlsEnabled = true
